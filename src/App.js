f31e378238d2c8aa5f2826b30ee5ddf9cc255a18
Shashwat Tripathi <97013903+shashwa7-dev@users.noreply.github.com>
2022-01-03 22:09:24 +0530

    BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
 import MicroSite from "./components/Microsite";
-import {BrowserRouter, Route,Switch} from 'react-router-dom'
+import {HashRouter, Route} from 'react-router-dom'
 
 
 class App extends React.Component {
   render(){
   return (
-   <BrowserRouter>
+   <HashRouter>
     <div className="App">
       <Route path="/phygitals/kit/">
         <MicroSite /> 
       </Route>
     </div>
-    </BrowserRouter>
+    </HashRouter>
   );
   }
 }
