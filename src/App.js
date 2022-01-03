14edd0e7509a05905ca5cbc6517c39973d18a3fb
shashwa7-dev <shashwa7.dev@gmail.com>
2022-01-03 18:00:03 +0530

    preloader update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import React from "react";
 import MicroSite from "./components/Microsite";
 
-function Loader(){
-  return(
-    <div className="preloader"></div>
-  )
-}
+// function Loader(){
+//   return(
+//     <div className="preloader"></div>
+//   )
+// }
 
 class App extends React.Component {
-
-  componentDidMount(){
-    document.querySelector('.preloader').style.display = 'none'
-  }
   render(){
   return (
     <div className="App">
-      <Loader />
-      <MicroSite/>
+      <MicroSite/> 
     </div>
   );
   }
